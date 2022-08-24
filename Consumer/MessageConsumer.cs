da2f42c809317bfe6f3b995bff50cb8f838446ae
Muhammet Eser <meser@nuevo.com.tr>
2022-08-24 16:57:28 +0300

    projenin oluşturulması ve uygulama yapılması

---
+++ Consumer/MessageConsumer.cs
@@ -0,0 +1,15 @@
+﻿using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumer
+{
+    public class MessageConsumer : IConsumer<IMessage>
+    {
+        public async Task Consume(ConsumeContext<IMessage> context)
+            => Console.WriteLine($"Gelen mesaj : {context.Message.Text}");
+    }
+}
---
+++ Consumer/Program.cs
@@ -0,0 +1,43 @@
+﻿using MassTransit;
+using Shared;
+using System;
+using System.Security.Authentication;
+using System.Threading.Tasks;
+
+namespace Consumer
+{ 
+    public class Message : IMessage
+    {
+        public string Text { get; set; }
+    }
+    public class MessageConsumer : IConsumer<IMessage>
+    {
+        public async Task Consume(ConsumeContext<IMessage> context)
+            => Console.WriteLine($"Gelen mesaj : {context.Message.Text}");
+    }
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string rabbitMqUri = "rabbitmq://localhost/";
+            string queue = "test-queue";
+
+            string userName = "guest";
+            string password = "123456";
+
+            var bus = Bus.Factory.CreateUsingRabbitMq(factory =>
+            {
+                factory.Host(rabbitMqUri, configurator =>
+                {
+                    configurator.Username(userName);
+                    configurator.Password(password);
+                });
+
+                factory.ReceiveEndpoint(queue, endpoint => endpoint.Consumer<MessageConsumer>());
+            });
+            await bus.StartAsync();
+            Console.ReadLine();
+            await bus.StopAsync();
+        }
+    }
+}
